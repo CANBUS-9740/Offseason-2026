e6558e28f074a2fc8d68bc7d177cc57be00533af
itay <itaykaikov@gmail.com>
2026-08-28 00:25:32 +0300

    had problems

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -3,8 +3,8 @@ package frc.robot.subsystems;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.*;
+import com.revrobotics.spark.config.LimitSwitchConfig;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.controller.PIDController;
@@ -18,27 +18,56 @@ public class TurretSubsystem extends SubsystemBase {
     private final SparkMax motor;
     private final RelativeEncoder encoder;
     private final DigitalInput forwardLimitSwitch;
-    private final DigitalInput middleLimitSwitch1;
-    private final DigitalInput middleLimitSwitch2;
-    private final PIDController pidController;
-    private final SparkMaxConfig config;
+    private final SparkLimitSwitch middleLimitSwitch1;
+    private final SparkLimitSwitch middleLimitSwitch2;
+    private final SparkClosedLoopController pidController;
+
 
 
     public TurretSubsystem(){
         motor = new SparkMax(RobotMap.TURRET_MOTOR_ID , SparkLowLevel.MotorType.kBrushless);
-        config = new SparkMaxConfig();
-        encoder = motor.getEncoder();
+        SparkMaxConfig config = new SparkMaxConfig();
         forwardLimitSwitch = new DigitalInput(RobotMap.TURRET_FORWARD_LIMIT_SWITCH_ID);
-        middleLimitSwitch1 = new DigitalInput(RobotMap.TURRET_MIDDLE_LIMIT_SWITCH_1_ID);
-        middleLimitSwitch2 = new DigitalInput(RobotMap.TURRET_MIDDLE_LIMIT_SWITCH_2_ID);
+        encoder = motor.getEncoder();
+        pidController = motor.getClosedLoopController();
+
+        config.closedLoop
+                .p(0)
+                .i(0)
+                .d(0)
+                .velocityFF(0)
+                .iZone(0)
+                .iMaxAccum(0) // max I accumulation ceiling
+                .dFilter(0)  // filter for D component output
+                .outputRange(-1, 1) // maximum output range
+                .feedbackSensor(ClosedLoopConfig);
+
+
+        config.limitSwitch
+                .forwardLimitSwitchEnabled(true)
+                .forwardLimitSwitchType(LimitSwitchConfig.Type.kNormallyOpen)
+                .reverseLimitSwitchEnabled(true)
+                .reverseLimitSwitchType(LimitSwitchConfig.Type.kNormallyOpen);
+
+        middleLimitSwitch1 = motor.getForwardLimitSwitch();
+        middleLimitSwitch2 = motor.getReverseLimitSwitch();
+
 
         motor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
         config.closedLoop.pid(RobotMap.TURRET_PID.kP,RobotMap.TURRET_PID.kI,RobotMap.TURRET_PID.kD);
         config.idleMode(SparkBaseConfig.IdleMode.kCoast);
+        motor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
 
-        pidController = new PIDController(0,0,0);
+
 
     }
+
+    public void moveToSetpoint(double setPoint) {
+        // configure the wanted setpoint to start the PID loop
+        pidController.setReference(setPoint, SparkBase.ControlType.kPosition);
+
+    }
+
 
     public void setMotor(double speed){
         motor.set(speed);
@@ -48,33 +77,26 @@ public class TurretSubsystem extends SubsystemBase {
         motor.stopMotor();
     }
 
+    public boolean isForwardLimitSwitch(){
+        return forwardLimitSwitch.get();
+    }
+
 
     public void setMoveToPosition(double positionDegrees){
-       pidController.setSetpoint(positionDegrees);
+        pidController.setSetpoint(positionDegrees);
     }
 
     public double getPositionDegrees(){
         return encoder.getPosition() * 360;
     }
 
-    public boolean isAlignedForward(){
-        return forwardLimitSwitch.get();
-    }
 
-    public boolean isAlignedMiddle1(){
-        return middleLimitSwitch1.get();
-    }
-
-    public boolean isAlignedMiddle2(){
-        return middleLimitSwitch2.get();
-    }
 
     public void periodic(){
         SmartDashboard.putNumber("turretPositionDegrees", getPositionDegrees());
-        SmartDashboard.putBoolean("turretForwardLimitSwitch" , isAlignedForward());
-        SmartDashboard.putBoolean("turretLimitSwitch1" , isAlignedMiddle1());
-        SmartDashboard.putBoolean("turretLimitSwitch2" , isAlignedMiddle2());
+
 
     }
+
 
 }
