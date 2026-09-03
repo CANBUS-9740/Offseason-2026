61cbcb52c8f3e5d910b863eaf9266443da3b1729
tbens <tbenshal@gmail.com>
2026-09-03 20:03:22 +0300

    full ShooterSystem and command requested

--- src/main/java/frc/robot/subsystems/PitcherSystem.java
+++ src/main/java/frc/robot/subsystems/PitcherSystem.java
@@ -7,10 +7,10 @@ import com.revrobotics.spark.*;
 import com.revrobotics.spark.config.LimitSwitchConfig;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
-
 
 
 public class PitcherSystem extends SubsystemBase {
@@ -22,50 +22,54 @@ public class PitcherSystem extends SubsystemBase {
     private final SparkClosedLoopController pidController;
 
 
-
-
-
-    public PitcherSystem(){
-        SparkMaxConfig config; //ez
-        config = new SparkMaxConfig();
+    public PitcherSystem() {
+        SparkMaxConfig config = new SparkMaxConfig();
         pitchermotor = new SparkMax(RobotMap.PITCHER_MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
-        config.absoluteEncoder //fixed mb
-                .positionConversionFactor(360 / RobotMap.PITCHER_GEARBOX_RATIO) //there is a gearbox on the motor so i kept the miltiplier and changed the place
-                .velocityConversionFactor(360 / RobotMap.PITCHER_GEARBOX_RATIO / 60); //same here
-        config.closedLoop.pid(RobotMap.PITCHER_PID.kP,RobotMap.PITCHER_PID.kI,RobotMap.PITCHER_PID.kD).outputRange(-1,1);
-        pitchermotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters); //moved
-        pidController = pitchermotor.getClosedLoopController();
-        toplimitswitch = pitchermotor.getForwardLimitSwitch();
-        bottomlimitswitch = pitchermotor.getReverseLimitSwitch();
+
+        config.closedLoop.pid(RobotMap.PITCHER_PID.kP, RobotMap.PITCHER_PID.kI, RobotMap.PITCHER_PID.kD).outputRange(-1, 1);
         config.limitSwitch
                 .forwardLimitSwitchType(LimitSwitchConfig.Type.kNormallyOpen)
                 .forwardLimitSwitchTriggerBehavior(LimitSwitchConfig.Behavior.kStopMovingMotor)
                 .reverseLimitSwitchType(LimitSwitchConfig.Type.kNormallyOpen)
                 .reverseLimitSwitchTriggerBehavior(LimitSwitchConfig.Behavior.kStopMovingMotor);
         config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+        pitchermotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+
         encoder = pitchermotor.getAbsoluteEncoder();
-
+        pidController = pitchermotor.getClosedLoopController();
+        toplimitswitch = pitchermotor.getForwardLimitSwitch();
+        bottomlimitswitch = pitchermotor.getReverseLimitSwitch();
     }
-    public void set(int speed){
-     pitchermotor.set(speed);
 
+    public void set(int speed) {
+        pitchermotor.set(speed);
     }
-    public boolean isUp(){
+
+    public boolean isUp() {
         return toplimitswitch.isPressed();
     }
-    public boolean isDown(){
-        return bottomlimitswitch.isPressed(); //changed
+
+    public boolean isDown() {
+        return bottomlimitswitch.isPressed();
     }
-    public double getPositionDegrees(){
-        return encoder.getPosition();
+
+    public double getPositionDegrees() {
+        return encoder.getPosition() * 360;
     }
-    public double getVelocityDegrees(){
-        return encoder.getVelocity();
+
+    public double getVelocityDegreesPerSecond() {
+        return Math.toDegrees(Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity()));
     }
-    public void setPositionToPitch(double angleDegrees){
+
+    public boolean isAtPosition(double angleDegrees) {
+        return Math.abs(getPositionDegrees() - angleDegrees) <= 1 && Math.abs(getVelocityDegreesPerSecond()) <= 1;
+    }
+
+    public void setPositionToPitch(double angleDegrees) {
         pidController.setSetpoint(angleDegrees, SparkMax.ControlType.kPosition);
     }
-    public void stop(){
+
+    public void stop() {
         pitchermotor.stopMotor();
     }
 
