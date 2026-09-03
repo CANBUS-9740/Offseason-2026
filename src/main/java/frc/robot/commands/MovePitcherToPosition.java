0a07b542bd478b64824b4629b1da2d03497a5b6a
Tartar371 <tbenshal@gmail.com>
2026-09-03 20:08:17 +0300

    added PitcherSystem and MovePitcherToPosition but didnt start the command me had headache sowwy(sorry) (#21)
    * added PitcherSystem and MovePitcherToPosition but didnd start the command me had headache sowwy(sorry)
    
    * added PitcherSystem and MovePitcherToPosition but didnd start the command me had headache sowwy(sorry)
    
    * fully fixed PitcherSystem and command moving to shooter!
    
    * full ShooterSystem and command requested
    
    * full ShooterSystem and command requested

---
+++ src/main/java/frc/robot/commands/MovePitcherToPosition.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.PitcherSystem;
+
+public class MovePitcherToPosition extends Command {
+
+    private final PitcherSystem pitcherSystem;
+    private final double targetAngleDegrees;
+
+    public MovePitcherToPosition(PitcherSystem pitcherSystem, double targetAngleDegrees) {
+        this.pitcherSystem = pitcherSystem;
+        this.targetAngleDegrees = targetAngleDegrees;
+        addRequirements(pitcherSystem);
+    }
+
+    @Override
+    public void initialize() {
+        pitcherSystem.setPositionToPitch(targetAngleDegrees);
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+       pitcherSystem.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pitcherSystem.isAtPosition(targetAngleDegrees);
+    }
+}
